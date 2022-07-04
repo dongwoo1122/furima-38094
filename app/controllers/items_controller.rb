21fd671aeb5ec75d6f7077bd9cedc5cc291648f1
dongwoo1122 <yoshikigota@gmail.com>
2022-07-04 16:52:18 +0900

    商品出品機能＃4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,11 +13,12 @@ class ItemsController < ApplicationController
       redirect_to root_path
     else
       render :new
+    end
   end
 
   private
   def item_params
     param.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :price, :delivery_fee_id, :address_region_id, :send_time_id).merge(user_id: current_user.id)
   end
-end
 
+end
